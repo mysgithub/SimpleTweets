b46f504befee0817a16888da482c36da96ce510f
Rokde <shyam.rokde@gmail.com>
2016-02-18 10:54:58 -0800

    Created Compose Activity

---
+++ app/src/main/java/com/codepath/apps/simpletweets/activities/ComposeActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.apps.simpletweets.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.codepath.apps.simpletweets.R;
+
+public class ComposeActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_compose);
+  }
+}
